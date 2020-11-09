04b3dfb81c67988d8424254e054c21b5468e323e
Anush Melqonyan <anush.melqonyan@org.fcc.am>
2020-11-09 18:14:39 +0400

    edit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from '@angular/router';
 import { CreateMovieComponent } from './create-movie/create-movie.component';
 import { MovieListComponent } from './movie-list/movie-list.component';
+import { UpdateMovieComponent } from './update-movie/update-movie.component';
 
 // localhost:4200/ -> movieListComponent
 // localhost:4200/add-movie -> CreateMovieListComponent
 
 const routes: Routes = [
     {path: '', component: MovieListComponent},
-    {path: 'add-movie', component: CreateMovieComponent}
+    {path: 'add-movie', component: CreateMovieComponent},
+    {path: 'edit-movie', component: UpdateMovieComponent}
+
 ]
 
 @NgModule({
