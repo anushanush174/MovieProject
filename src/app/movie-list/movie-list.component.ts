f487e5ef52c27d4908ca1e606149a3b45ea488f7
AnushMelkonyan <melkonyananushbk@gmail.com>
2020-11-22 21:01:01 +0400

    push

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -10,21 +10,12 @@ import { Movie, MovieService } from './movie.service';
 export class MovieListComponent implements OnInit {
   public movieList: Movie[];
   public currentPage = 1;
-  editMovieForm;
 
   constructor(
     private movieService: MovieService,
     ) {}
 
   ngOnInit(): void {
-    this.editMovieForm = new FormGroup( {
-      title: new FormControl(''),
-      year: new FormControl(''),
-      actors: new FormControl(''),
-      duration: new FormControl(''),
-      // genres: new FormControl(''),
-      // storyline: new FormControl(''),
-    });
     this.movieService.getMovieList();
     this.getMovies();
   }
@@ -34,19 +25,6 @@ export class MovieListComponent implements OnInit {
       this.movieService.getRatingAvarage(data);
       this.movieList = data;
     });
-  }
-
-  onClickEditButton(currentMovie): void{
-    // const formData = { ...this.editMovieForm.value };
-    this.editMovieForm.setValue({
-      title: currentMovie.title.toString() ,
-      year: currentMovie.year,
-      actors: currentMovie.actors,
-      duration: currentMovie.duration,
-      // genres: currentMovie.genres,
-      // storyline: currentMovie.description,
-    });
-    console.log(currentMovie);
   }
 
   previewPage(): void {
--- src/app/update-movie/update-movie.component.ts
+++ src/app/update-movie/update-movie.component.ts
@@ -1,40 +1,54 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { MovieService } from '../movie-list/movie.service';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Movie, MovieService } from '../movie-list/movie.service';
 
 @Component({
   selector: 'app-update-movie',
   templateUrl: './update-movie.component.html',
-  styleUrls: ['./update-movie.component.css']
+  styleUrls: ['./update-movie.component.css'],
 })
 export class UpdateMovieComponent implements OnInit {
+  private routeSub: Subscription;
   public editMovieForm: FormGroup;
-  constructor(private movieService: MovieService) { }
+
+  constructor(
+    private movieService: MovieService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.editMovieForm = new FormGroup( {
+    this.editMovieForm = new FormGroup({
       title: new FormControl(''),
       year: new FormControl(''),
       actors: new FormControl(''),
       duration: new FormControl(''),
-      raiting: new FormControl(''),
       genres: new FormControl(''),
       description: new FormControl(''),
     });
+
+    this.editMovieForm.setValue({
+      title: 'anush',
+      year: '1996',
+      actors: 'melkonyan',
+      duration: '24 tari',
+      genres: '',
+      description: 'My Movie Description',
+    });
+
+    this.routeSub = this.route.params.subscribe((params) => {
+      const id = params.id;
+      this.movieService.subject.subscribe((data: Movie[]) => {
+        this.movieService.getRatingAvarage(data);
+        data.map((currentMovie) => {
+          console.log('currentMovie.id', currentMovie.id);
+        });
+      });
+    });
+
   }
 
-  onEdit(): void {
-    // const formData = { ...this.editMovieForm.value };
-    // this.editMovieForm.setValue({
-    //   title: formData.title,
-    //   year: new FormControl(''),
-    //   actors: new FormControl(''),
-    //   duration: new FormControl(''),
-    //   raiting: new FormControl(''),
-    //   genres: new FormControl(''),
-    //   description: new FormControl(''),
-    // });
-    // console.log(formData);
-  }
+  onEdit(): void {}
 
 }
